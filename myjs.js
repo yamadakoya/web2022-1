54329897fa89edb3c4a98437afef4de2f6046787
yamadakoya <131091958+yamadakoya@users.noreply.github.com>
2023-06-27 10:30:56 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -2,5 +2,10 @@ alert('Hello, Osamu!);
 alert(1+1);
 alert(Match.sqrt(1+1));
 alert(Match.sqrt(2+3));
-alert(1/0)
-alert(0/0)
+alert(1/0);
+alert(0/0);
+alert(2+3);
+alert('2'+'3');
+alert(Number('2')+Number('3');
+alert('2'+3);
+alert(('2')+3);
