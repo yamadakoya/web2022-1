a696906b5c97e062367e3c370b278995f079d48c
yamadakoya <131091958+yamadakoya@users.noreply.github.com>
2023-07-04 10:46:00 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,6 +1,6 @@
-alert('Hello, Koya!');
-alert(1+1);
-alert(Math.sqrt(1+1));
+//alert('Hello, Koya!');
+//alert(1+1);
+/*alert(Math.sqrt(1+1));*/
 alert(Math.sqrt(2+3));
 //alert(1/0);//
 //alert(0/0);//
